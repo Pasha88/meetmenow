938d9a7bd416ccdf1938cb9914832d8b35b24ca4
Pasha88 <ukrwar2017@gmail.com>
2018-06-25 23:09:47 +0300

    second

--- database/migrations/2016_09_11_113843_create_comments_table.php
+++ database/migrations/2016_09_11_113843_create_comments_table.php
@@ -18,13 +18,9 @@ class CreateCommentsTable extends Migration
             $table->string('email');
             $table->text('comment');
             $table->boolean('approved');
-            $table->integer('post_id')->unsigned();
             $table->timestamps();
         });
 
-        Schema::table('comments', function ($table){
-            $table->foreign('post_id')->references('id')->on('posts')->onDelete('cascade');
-        });
     }
 
     /**
